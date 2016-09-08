a0ff928bd0a56339d9e4aeb262ba52a128dbe430
Maksim Solovianov <maxsolovianov@est.local>
2016-09-08 15:34:51 +0300

    Убрал пароль

--- src/test/java/Package/Constants.java
+++ src/test/java/Package/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final String LOGIN_PATH = "//*[@id=\"UserName\"]";
     public static final String DEFAULT_LOGIN = "maxsolovianov";
     public static final String PASSWORD_PATH = "//*[@id=\"Password\"]";
-    public static final String CORRECT_PASSWORD = "Vector,123L";
+    public static final String CORRECT_PASSWORD = "";
     public static final String SIGN_UP_BUTTON = "//*[@id=\"login_form\"]/div[3]/div[2]/button/b";
     public static final String MENU_PATH = ".//*[@id='pass_form']/div[1]/span/span/span[1]";
     public static final String MENU_ELEMENT_PATH = ".//*[@id='CompanyId_listbox']/li[8]";
